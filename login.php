3a5932ad2a45a5c2588ce10b4c581ed37d9ea905
Edouard Burel <bureledo@gmail.com>
2023-09-30 14:53:21 +0200

    check ok

--- login.php
+++ login.php
@@ -93,7 +93,7 @@ if (isset($_POST['submit'])) {
                 <input name="email" type="text" class="form-control" placeholder="Username" required value="<?php echo htmlspecialchars($_POST['email'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"><br>
                 <input name="password" type="password" class="form-control" placeholder="Password" required value="<?php echo htmlspecialchars($_POST['password'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"><br>
                 <div class="btn-login">
-                    <button name="submit" type="submit" class="btn">Loginus</button>
+                    <button name="submit" type="submit" class="btn">Login</button>
                 </div>
             </form>
         </div>
